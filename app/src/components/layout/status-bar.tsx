460523866e9c44a3ab35e276f32f6f1d26c0df1e
porkytheblack <porkytheblack@gmail.com>
2026-02-06 15:05:28 +0300

    chore: bump version to v0.1.4

--- app/src/components/layout/status-bar.tsx
+++ app/src/components/layout/status-bar.tsx
@@ -137,7 +137,7 @@ export function StatusBar({ breadcrumbs = [], statusMessage }: StatusBarProps) {
             </span>
           )}
           <span className="text-coco-text-tertiary font-mono text-[10px]">
-            v0.1.3
+            v0.1.4
           </span>
         </div>
       </div>
